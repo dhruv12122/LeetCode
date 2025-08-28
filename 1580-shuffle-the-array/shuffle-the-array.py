ed8fa46b65366b0fea3b895509cdf27a135fb85b
Dhruv Sable <140090385+dhruv12122@users.noreply.github.com>
2025-08-28 13:41:51 +0530

    Time: 37 ms (74.14%) | Memory: 12.6 MB (43.46%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        n1 = nums[0:n]
+        n2 = nums[n:]
+        n3 = []
+        for i in range(n):
+            n3.append(n1[i])
+            n3.append(n2[i])
+        return n3
